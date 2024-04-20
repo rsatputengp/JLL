abb0dd9dd3825ae5c2007178bb35344a322773b6
ritik <ritik@ritik7pute>
2024-04-20 15:59:15 +0530

    Updated Code

--- src/main/resources/static/js/SD_User_dashboard.js
+++ src/main/resources/static/js/SD_User_dashboard.js
@@ -57,51 +57,50 @@ app.controller('SD_User_Controller', function ($scope, $http, $document) {
         };
 
 
-        $scope.submitITform = function () {
 
-            $scope.insuranceTracker = {
-                region: $scope.region,
-                branchCode: $scope.branchCode,
-                branchName: $scope.branchName,
-                claimId: $scope.claimId,
-                centerId: $scope.centerId,
-                centerName: $scope.centerName,
-                clientId: $scope.clientId,
-                accountId: $scope.accountId,
-                disbursementDate: $scope.disbursementDate,
-                clientName: $scope.clientName,
-                nomineeName: $scope.nomineeName,
-                deathClientName: $scope.deathClientName,
-                disbursementAmount: $scope.disbursementAmount,
-                emiDay: $scope.emiDay,
-                dateOfDeath: $scope.dateOfDeath,
-                deathReasion: $scope.deathReasion,
-                paidEmi: $scope.paidEmi,
-                loanOutstandingAmt: $scope.loanOutstandingAmt,
-                otsAmt: $scope.otsAmt,
-                claimSettelmentAmt: $scope.claimSettelmentAmt,
-                memberHandoverAmount: $scope.memberHandoverAmount,
-                claimStatus: $scope.claimStatus,
-                remarks: $scope.remarks,
-                trueCellPunchingDate: $scope.trueCellPunchingDate,
-                datedOfDOCReceivedFromMember: $scope.datedOfDOCReceivedFromMember,
-                datedOfSendDocToHo: $scope.datedOfSendDocToHo,
-                datedOfSendDocToKotak: $scope.datedOfSendDocToKotak,
-                dateOfSettelmentByKotak: $scope.dateOfSettelmentByKotak,
-                accountCloseDateByBranch: $scope.accountCloseDateByBranch,
-                incentiveReceivedInMonth: $scope.incentiveReceivedInMonth
-            };
 
-            var URL = $scope.uRl + "insurancetrackers/create";
+        $scope.getITformData = function (id) {
+
+            var URL = $scope.uRl + "insurancetrackers/get/" + id;
             $http.post(URL, $scope.insuranceTracker)
                     .then(function (response) {
-                        console.log(response);
-                        alert("Form submitted Successfully");
+                        $scope.record = response.data;
+                        $scope.region = $scope.record.region;
+                        $scope.branchCode = $scope.record.branchCode;
+                        $scope.branchName = $scope.record.branchName;
+                        $scope.claimId = $scope.record.claimId;
+                        $scope.centerId = $scope.record.centerId;
+                        $scope.centerName = $scope.record.centerName;
+                        $scope.clientId = $scope.record.clientId;
+                        $scope.accountId = $scope.record.accountId;
+                        $scope.disbursementDate = $scope.record.disbursementDate;
+                        $scope.clientName = $scope.record.clientName;
+                        $scope.nomineeName = $scope.record.nomineeName;
+                        $scope.deathClientName = $scope.record.deathClientName;
+                        $scope.disbursementAmount = $scope.record.disbursementAmount;
+                        $scope.emiDay = $scope.record.emiDay;
+                        $scope.dateOfDeath = $scope.record.dateOfDeath;
+                        $scope.deathReasion = $scope.record.deathReasion;
+                        $scope.paidEmi = $scope.record.paidEmi;
+                        $scope.loanOutstandingAmt = $scope.record.loanOutstandingAmt;
+                        $scope.otsAmt = $scope.record.otsAmt;
+                        $scope.claimSettelmentAmt = $scope.record.claimSettelmentAmt;
+                        $scope.memberHandoverAmount = $scope.record.memberHandoverAmount;
+                        $scope.claimStatus = $scope.record.claimStatus;
+                        $scope.remarks = $scope.record.remarks;
+                        $scope.trueCellPunchingDate = $scope.record.trueCellPunchingDate;
+                        $scope.datedOfDOCReceivedFromMember = $scope.record.datedOfDOCReceivedFromMember;
+                        $scope.datedOfSendDocToHo = $scope.record.datedOfSendDocToHo;
+                        $scope.datedOfSendDocToKotak = $scope.record.datedOfSendDocToKotak;
+                        $scope.dateOfSettelmentByKotak = $scope.record.dateOfSettelmentByKotak;
+                        $scope.accountCloseDateByBranch = $scope.record.accountCloseDateByBranch;
+                        $scope.incentiveReceivedInMonth = $scope.record.incentiveReceivedInMonth;
+
                     }, function (error) {
                         console.log(error);
                     });
-
         };
+
     } else {
         window.location.href = $scope.uRl + "index.html";
     }
